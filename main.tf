cc0310ea45338993e978a5dd76ac16f6abc0dc08
Dennis Huynh <dennis.huynh@galvanize.com>
2023-05-08 12:51:57 -0700

    Manual delete

--- main.tf
+++ main.tf
@@ -15,25 +15,7 @@ provider "aws" {
 
 ## Create an S3 bucket
 
-resource "aws_s3_bucket" "example" {
-  bucket = "akash-my-tf-test-bucket"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 
-resource "aws_s3_object" "object" {
-  #bucket = "akash-my-tf-test-bucket"
-  bucket=aws_s3_bucket.example.id
-  key    = "sample.txt"
-  source = "build/libs/sample.txt"
-  content_type = "text/csv"
 
-  # The filemd5() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-  # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("build/libs/sample.txt")
-}
-
+## Create an S3 object to upload "./build/libs/sample.txt"
